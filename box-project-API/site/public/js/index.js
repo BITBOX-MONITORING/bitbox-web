a18ea39696c3daf957f5ae6cef119335c06db66c
helloWorldRuan <ruan.rodrigues@sptech.school>
2023-03-05 13:30:49 -0300

    Adicionando blur effects, atualizando tela de cadastro

--- box-project-API/site/public/js/index.js
+++ box-project-API/site/public/js/index.js
@@ -1,3 +1,8 @@
+window.addEventListener('scroll', function(){
+    var fixedNavbar = document.querySelector('header')
+    fixedNavbar.classList.toggle('sticky', window.scrollY > 60)
+})
+
 function redirectToLogin() {
     window.location.href = "sign-page.html"
 }
