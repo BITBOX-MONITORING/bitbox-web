09eabb3a598683b85cc137255c3a93eb02b67bda
helloWorldRuan <ruan.rodrigues@sptech.school>
2023-03-11 14:01:10 -0300

    Aplicando responsividade seção de Vantagens e Atuação

--- box-project/site/public/js/index.js
+++ box-project/site/public/js/index.js
@@ -1,8 +1,10 @@
+// Navbar fixa
 window.addEventListener('scroll', function(){
     var fixedNavbar = document.querySelector('header')
     fixedNavbar.classList.toggle('sticky', window.scrollY > 60)
 })
 
+// Link para o login
 function redirectToLogin() {
     window.location.href = "sign-page.html"
 }
