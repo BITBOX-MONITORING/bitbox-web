ddab959ba58dd7356e3698fdc39d1fafe2e3a6f8
helloWorldRuan <ruan.rodrigues@sptech.school>
2023-05-11 01:30:37 -0300

    [feature] insert mock charts in device modal

--- site/public/js/devices.js
+++ site/public/js/devices.js
@@ -1,71 +1,80 @@
 const devices = [
-  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Elma Maria", status: "alert" },
+  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Elma Maria", status: "alerta" },
   { id: "BD0987", img: "", type: "WINDOWS", name: "WINDOWS 10", user: "Josimar Mar", status: "ok" },
-  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Epaminondas Lúcio", status: "danger" },
-  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Catarina Marica", status: "danger" },
-  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Thiago Gomes", status: "danger" },
+  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Epaminondas Lúcio", status: "crítico" },
+  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Catarina Marica", status: "crítico" },
+  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Thiago Gomes", status: "crítico" },
   { id: "BD0987", img: "", type: "WINDOWS", name: "WINDOWS 10", user: "Fernananda Caramico", status: "ok" },
-  { id: "BD0987", img: "", type: "WINDOWS", name: "WINDOWS 10", user: "Paulo Mimoso", status: "alert" },
-  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Giuseppe Casseca", status: "danger" },
-  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Simas Turbo", status: "danger" },
+  { id: "BD0987", img: "", type: "WINDOWS", name: "WINDOWS 10", user: "Paulo Mimoso", status: "alerta" },
+  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Giuseppe Casseca", status: "crítico" },
+  { id: "BD0987", img: "", type: "LINUX", name: "UBUNTU 22.2", user: "Simas Turbo", status: "crítico" },
 ];
+
+const statusColors = {
+  ok: "#3ad13a",
+  alerta: "#f5e65b",
+  crítico: "#d13a47",
+};
 
 function buildModal(device) {
 
-  const modal = ` <div class="card">
-  <div class="img-device">
-      <img src="${device.img}" alt="">
-  </div>
+  const colorStatus = statusColors[device.status];
 
-  <div class="info">
-      <span>${device.id}</span>
-      <h1>${device.name}</h1>
-      <h3>${device.user}</h3>
-  </div>
-</div>
-
-<div class="device-kpi">
-  <div class="kpi">
-      <div class="title">
-          <i class="ph ph-cpu"></i>
-          <span> CPU </span>
+  const modal = ` 
+    <div class="card">
+      <div class="img-device">
+          <img src="${device.img}" alt="">
       </div>
-      <h1>15%</h1>
-  </div>
 
-  <div class="wall"></div>
-
-  <div class="kpi">
-      <div class="title">
-          <i class="ph-rocket-launch"></i>
-          <span>RAM</span>
+      <div class="info">
+          <span>${device.id}</span>
+          <h1>${device.name}</h1>
+          <h3>${device.user}</h3>
       </div>
-      <h1>4.8 <em>GB</em></h1>
-  </div>
-
-  <div class="wall"></div>
-
-  <div class="kpi">
-      <div class="title">
-          <i class="ph ph-disc"></i>
-          <span>DISCO</span>
       </div>
-      <h1>49%</h1>
-  </div>
 
-  <div class="wall"></div>
+      <div class="device-kpi">
+      <div class="kpi">
+          <div class="title">
+              <i class="ph ph-cpu"></i>
+              <span> CPU </span>
+          </div>
+          <h1>15%</h1>
+      </div>
 
-  <div class="kpi">
-      <div class="title">
-          <i class="ph-duotone ph-warning"></i>
-          <span>STATUS</span>
+      <div class="wall"></div>
+
+      <div class="kpi">
+          <div class="title">
+              <i class="ph-rocket-launch"></i>
+              <span>RAM</span>
+          </div>
+          <h1>4.8 <em>GB</em></h1>
       </div>
-      <div class="status-info">
-          <div class="status"></div>
-          <h1>OK</h1>
+
+      <div class="wall"></div>
+
+      <div class="kpi">
+          <div class="title">
+              <i class="ph ph-disc"></i>
+              <span>DISCO</span>
+          </div>
+          <h1>49%</h1>
       </div>
-  </div>
-</div>`
+
+      <div class="wall"></div>
+
+      <div class="kpi">
+          <div class="title">
+              <i class="ph-duotone ph-warning"></i>
+              <span>STATUS</span>
+          </div>
+          <div class="status-info">
+              <div style="background-color: ${colorStatus}" class="status"></div>
+              <h1 style="color: ${colorStatus}">${device.status}</h1>
+          </div>
+      </div>
+    </div>`
 
   return modal
 }
@@ -73,16 +82,10 @@ function buildModal(device) {
 function buildCardDevice(device, index) {
   device.img = device.type === "LINUX" ? "assets/device-linux.png" : "assets/device-windows.png";
 
-  const statusColors = {
-    ok: "#3ad13a",
-    alert: "#f5e65b",
-    danger: "#d13a47",
-  };
-
   const colorStatus = statusColors[device.status];
 
   const cardDevice = `
-  <div class="card" onclick="openDeviceModal(${index})">
+    <div class="card" onclick="openDeviceModal(${index})">
       <div class="img-device">
       <img src="${device.img}" alt="">
       </div>
@@ -94,7 +97,7 @@ function buildCardDevice(device, index) {
       </div>
 
       <div class="status" style="background-color:${colorStatus}"></div>
-  </div>`;
+    </div>`;
 
   return cardDevice
 }
@@ -109,16 +112,16 @@ document.addEventListener("DOMContentLoaded", () => {
   modal = document.querySelector(".device-info")
 
   devices.sort(function (a, b) {
-    if (a.status === "danger" && b.status !== "danger") {
+    if (a.status === "crítico" && b.status !== "crítico") {
       return -1;
     }
-    if (b.status === "danger" && a.status !== "danger") {
+    if (b.status === "crítico" && a.status !== "crítico") {
       return 1;
     }
-    if (a.status === "alert" && b.status !== "alert" && b.status !== "danger") {
+    if (a.status === "alerta" && b.status !== "alerta" && b.status !== "crítico") {
       return -1;
     }
-    if (b.status === "alert" && a.status !== "alert" && a.status !== "danger") {
+    if (b.status === "alerta" && a.status !== "alerta" && a.status !== "crítico") {
       return 1;
     }
     return 0;
@@ -154,6 +157,22 @@ function onClickOutsideModal(event) {
 function openDeviceModal(index) {
   const statusBar = document.querySelector(".status-bar")
   const device = devices[index];
+
+  let device_data = {
+    cpu: [],
+    ram: [],
+    disco: [],
+    abas: []
+  }
+  
+  for (let i = 0; i < 6; i++){
+    device_data.cpu.push(parseInt(Math.random() * 100))
+    device_data.ram.push(parseInt(Math.random() * 8))
+    device_data.disco.push(parseInt(Math.random() * 500))
+    device_data.abas.push(parseInt(Math.random() * 20))
+  }
+
+  localStorage.setItem('data', JSON.stringify(device_data))
 
   modal.style = modalStyle.opened;
   statusBar.innerHTML = buildModal(device)
